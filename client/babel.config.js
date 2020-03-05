9a722aa26a9dd70b92d32d159cc271795b86f4aa
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-05 15:06:39 -0500

    Begins construction of React-Native based client

---
+++ client/babel.config.js
@@ -0,0 +1,14 @@
+{
+    plugins: [
+  +    [
+  +       'module-resolver',
+  +       {
+  +         root: ['./src'],
+  +         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+  +         alias: {
+  +           "test/*": ["./test/"],
+  +         }
+  +       }
+  +     ]
+    ]
+  }
