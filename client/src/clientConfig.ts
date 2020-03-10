89eff698c92b6ff98bb62620bbeadc7a9f962d3c
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-10 17:28:39 -0400

    Refactors project to support both web and mobile clients

--- client/src/clientConfig.ts
+++
@@ -1,8 +0,0 @@
-const apiId = 'ewuk0tjwpj';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
-
-export const authConfig = {
-  domain: 'dev-rf-u05fp.auth0.com',
-  clientId: 'qQAsD4aGaOdg58ovHdQ7WWIRf4KTtdcg',
-  callbackUrlWeb: 'http://localhost:3000/callback',
-};
