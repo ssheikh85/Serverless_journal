1e5151a371c644077f51630bed2ef4f8d3e41f8d
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-11 16:19:50 -0400

    Adds apollo client for grpahql integration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import ApolloProvider from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
+import {ApolloLink} from 'apollo-link';
 import Root from '../components/root';
 import {apiEndpoint} from '../client_config';
 
+//Apollo Client set-up
 const client = new ApolloClient({
-  uri: apiEndpoint,
+  link: ApolloLink.from([
+    onError(({graphQLErrors, networkError}) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({message, locations, path}) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: apiEndpoint,
+      credentials: 'same-origin',
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
 
-const App = () => {
+const App = () => (
   <ApolloProvider client={client}>
     <>
       <Root />
     </>
-  </ApolloProvider>;
-};
+  </ApolloProvider>
+);
 
 export default App;
