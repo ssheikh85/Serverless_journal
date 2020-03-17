dfde4d302bdcb5740072e72f07189c1147aab157
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-17 17:21:38 -0400

    Adds an AddEntry component and addresses issues with Hooks

---
+++ client/src/components/AddEntry.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import {useMutation} from '@apollo/react-hooks';
+import {ADD_ENTRY_M} from '../graphql-api/entries_api';
+
+//Component to add an entry
+export const AddEntry = (props: any) => {
+  const [inputNewContent, setInputNewContent] = useState({
+    content: '',
+  });
+  const {userId} = props;
+
+  const [createEntry, {data}] = useMutation(ADD_ENTRY_M);
+  return (
+    <View>
+      <Text>Add a new Entry</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to create a new entry"
+        onChangeText={text => setInputNewContent({content: text})}
+        value={inputNewContent.content}
+      />
+
+      <Button
+        title="Add a new entry"
+        onPress={() => {
+          createEntry({variables: {userId, inputNewContent}});
+        }}></Button>
+    </View>
+  );
+};
--- client/src/components/Entries.tsx
+++ client/src/components/Entries.tsx
@@ -2,15 +2,15 @@ import React, {useEffect, useState, useRef} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  ScrollView,
   Text,
-  TextInput,
   FlatList,
-  Button,
   Platform,
 } from 'react-native';
-import {useQuery, useMutation} from '@apollo/react-hooks';
-import {GET_ENTRIES_Q, ADD_ENTRY_M} from '../graphql-api/entries_api';
+import {useQuery} from '@apollo/react-hooks';
+import {GET_ENTRIES_Q} from '../graphql-api/entries_api';
 import {SingleEntryItem} from '../components/SingleEntryItem';
+import {AddEntry} from '../components/AddEntry';
 import {EntryItem} from '../models_requests/EntryItem';
 import authHandlerMobile from '../auth/authHandlerMobile';
 import {useAuth0} from '../auth/authHandlerWeb';
@@ -18,9 +18,6 @@ import {useAuth0} from '../auth/authHandlerWeb';
 //List of entries
 export const Entries = (props: any) => {
   const [userId, setUserId] = useState('');
-  const [inputNewContent, setInputNewContent] = useState({
-    content: '',
-  });
   const [entries, setEntries] = useState([]);
 
   const {user} = useAuth0();
@@ -43,15 +40,15 @@ export const Entries = (props: any) => {
     getUserId();
   }
 
-  const setEntrieesFromServer = (userId: string) => {
-    const {data, error} = useQuery(GET_ENTRIES_Q, {
-      variables: {userId},
-    });
-    if (error) {
-      alert(`An error has occurred ${error.message}`);
-    }
+  const {data, error} = useQuery(GET_ENTRIES_Q, {
+    variables: {userId},
+  });
+
+  if (error) {
+    alert(`An error has occurred ${error.message}`);
+  } else {
     setEntries(data);
-  };
+  }
 
   const usePrevious = (value: any) => {
     const ref = useRef();
@@ -61,40 +58,26 @@ export const Entries = (props: any) => {
     return ref.current;
   };
 
+  const prevValues = usePrevious(entries) as unknown;
+  const prevEntries = prevValues as [];
+
   useEffect(() => {
-    const prevValues = usePrevious(entries) as unknown;
-    const prevEntries = prevValues as [];
-
     if (entries.length !== prevEntries.length) {
-      setEntrieesFromServer(userId);
     }
-    setEntrieesFromServer(userId);
   });
-
-  const [createEntry, {data}] = useMutation(ADD_ENTRY_M);
 
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.header}>Your Entries</Text>
-        <Text>Add a new Entry</Text>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to create a new entry"
-          onChangeText={text => setInputNewContent({content: text})}
-          value={inputNewContent.content}
-        />
-
-        <Button
-          title="Add a new entry"
-          onPress={() => {
-            createEntry({variables: {userId, inputNewContent}});
-          }}></Button>
-        <FlatList
-          data={entries as EntryItem[]}
-          renderItem={({item}) => <SingleEntryItem content={item} />}
-          keyExtractor={item => item.entryId}
-        />
+        <ScrollView style={styles.scrollView}>
+          <AddEntry userID={userId} />
+          <Text style={styles.header}>Your Entries</Text>
+          <FlatList
+            data={entries as EntryItem[]}
+            renderItem={({item}) => <SingleEntryItem content={item} />}
+            keyExtractor={item => item.entryId}
+          />
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -107,6 +90,10 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    marginHorizontal: 20,
   },
   header: {
     fontSize: 32,
