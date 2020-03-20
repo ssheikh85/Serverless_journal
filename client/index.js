f258372df2f512faec4f0c8dd233b48c67153213
Saeed Sheikh <saeedshaikh166@gmail.com>
2020-03-20 12:55:44 -0400

    Auth working for mobile

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
-import App from '../client/src/mobile/App';
+import App from './src/mobile/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
